b225d7a05b128827bedf6a170093c9fcbeefd50b
朱伟彬 <627503428@qq.com>
2023-08-29 14:54:41 +0800

    feat: test commitlint

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,6 @@ function App() {
 	return (
 		<>
 			<div>
-
-
-
-
 				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
@@ -23,14 +19,9 @@ function App() {
 						alt="React logo"
 					/>
 				</a>
-
 			</div>
 			<h1>Vite + React</h1>
-
-
-
 			<div className="card">
-				
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
